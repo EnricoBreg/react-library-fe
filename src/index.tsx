530f9e588a93c9a4a8c044f4fc918afe19412a74
Enrico <bregolienrico@gmail.com>
2024-08-08 16:10:48 +0200

    Project Fix: rimosso bundle bootstrap da public/assets ed aggiunto con npm al progetto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap JS Bundle
+import "bootstrap/dist/js/bootstrap.bundle";
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(
