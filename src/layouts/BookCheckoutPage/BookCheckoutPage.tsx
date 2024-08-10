994c7ee25a075d6abdb743010d20229eec2cf020
Enrico <bregolienrico@gmail.com>
2024-08-10 12:23:21 +0200

    [127 checkout page] Aggiunta prima versione della pagina di informazioni di un libro

---
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Commons/SpinnerLoading";
+
+export const BookCheckoutPage = () => {
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const bookId = window.location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const url = `http://localhost:8080/api/books/${bookId}`;
+
+      const response = await fetch(url);
+
+      if (!response.ok) throw new Error("Something went wrong :(");
+
+      const responseJSON: BookModel = await response.json();
+      console.log(responseJSON);
+
+      setBook(responseJSON);
+      setIsLoading(false);
+    };
+
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) return <SpinnerLoading />;
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="container d-none d-lg-block">
+        <div className="row mt-5">
+          {/* immagine */}
+          <div className="col-sm-2 col-md-2">
+            {book?.img ? (
+              <img src={book.img} width="226" height="349" alt={book.title} />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
+          </div>
+          {/* titolo - autore - descrizione */}
+          <div className="container col-4 col-md-4">
+            <div className="ml-2">
+              <h2>{book?.title}</h2>
+              <h5 className="text-primary">{book?.author}</h5>
+              <p className="lead">{book?.description}</p>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="container d-lg-none mt-5">
+        <div className="d-flex justify-content-center align-items-center">
+          {book?.img ? (
+            <img src={book.img} width="226" height="349" alt={book.title} />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
+        </div>
+        <div className="mt-4">
+          <div className="ml-2">
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
